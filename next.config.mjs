1f49998a0f5d96eb1e42f3befa4dfa7202f56d7a
paragonxdxbv-prog <paragonxdxbv@gmail.com>
2025-09-14 14:17:07 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
